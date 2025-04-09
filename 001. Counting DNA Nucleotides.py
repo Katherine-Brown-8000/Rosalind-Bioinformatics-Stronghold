df5c407f3d4b2848503181a91e5682c907765a15
Katherine Brown <155707632+Katherine-Brown-8000@users.noreply.github.com>
2025-04-09 12:24:20 -0400

    Update 001. Counting DNA Nucleotides.py

--- 001. Counting DNA Nucleotides.py
+++ 001. Counting DNA Nucleotides.py
@@ -2,7 +2,16 @@
 
 nucleotides = ["A", "C", "G", "T"]
 
-DNA_sample = "ACGT"
+# You might want to change the name of the rosalind file because it can get confusing, here I changed it to rosalind_dna_1
+file_name = r"C:\Users\scien\Downloads\rosalind_dna_1.txt"
+
+def read_file(file_name):
+    with open(file_name, 'r') as file:
+        content = file.readlines()
+        sequence = "".join([line.strip() for line in content if not line.startswith('>')])
+    return sequence
+
+sequence = read_file(file_name)
 
 def nucleotide_count(seq):
     nucleotides_calculator = {"A": 0, "C": 0, "G": 0, "T": 0}
@@ -10,4 +19,5 @@ def nucleotide_count(seq):
         nucleotides_calculator[num] += 1
     return nucleotides_calculator
 
-print(nucleotide_count(DNA_sample))
+# For the solution type A, C, G, and T in that order, it will be marked wrong if :"{} are included
+print(nucleotide_count(sequence))
